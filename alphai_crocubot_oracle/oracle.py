f861c52237cbca03b2a1289a6f4949cef02bf264
Fergus Simpson <fergus.simpson@alpha-i.co>
2017-10-20 15:13:49 +0100

    Dumps predict data

--- alphai_crocubot_oracle/oracle.py
+++ alphai_crocubot_oracle/oracle.py
@@ -227,6 +227,9 @@ class CrocubotOracle:
             covariance = pd.DataFrame(data=forecast_covariance, columns=predict_data['close'].columns,
                                       index=predict_data['close'].columns)
 
+        logging.info("Dump of predict data: {}".format(predict_data))
+        logging.info("Dump of predict x: {}".format(predict_x))
+
         return means, covariance
 
     def filter_nan_samples(self, train_x, train_y):
