181d6896a25d9fe19ec1caf034afc4cdb8f2d0b4
Fergus Simpson <fergus.simpson@alpha-i.co>
2017-10-24 16:41:06 +0100

    Changed default to Adam due to better backtest convergence

--- alphai_crocubot_oracle/flags.py
+++ alphai_crocubot_oracle/flags.py
@@ -12,7 +12,7 @@ def set_training_flags(config):
     tf.flags._global_parser = argparse.ArgumentParser()
 
     if 'optimisation_method' not in config:
-        config['optimisation_method'] = 'GDO'
+        config['optimisation_method'] = 'Adam'
 
     tf.app.flags.DEFINE_boolean('predict_single_shares', config['predict_single_shares'],
                                 """Whether the network predicts one share at a time.""")
