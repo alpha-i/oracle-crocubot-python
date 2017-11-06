f245e8a9f41d1693a70ecc2320adffef6ba49aac
Daniele Murroni <daniele.murroni@gmail.com>
2017-11-06 17:11:52 +0000

    benchmar on time series

--- examples/benchmark/time_series.py
+++ examples/benchmark/time_series.py
@@ -1,18 +1,20 @@
 import datetime
-from timeit import default_timer as timer
 
 import numpy as np
 import tensorflow as tf
-from alphai_data_sources.data_sources import DataSourceGenerator
-from alphai_data_sources.generator import BatchOptions
 from alphai_time_series.performance_trials.performance import Metrics
 
 from alphai_crocubot_oracle import iotools as io
-from alphai_crocubot_oracle.crocubot import train_with_datasource, evaluate as eval
+import alphai_crocubot_oracle.crocubot.evaluate as crocubot_eval
+import alphai_crocubot_oracle.crocubot.train as crocubot_train
+from alphai_crocubot_oracle.crocubot.helpers import TensorflowPath, TensorboardOptions
 from alphai_crocubot_oracle.crocubot.model import CrocuBotModel
 from alphai_crocubot_oracle.data.classifier import BinDistribution
 from alphai_crocubot_oracle.data.providers import TrainDataProviderForDataSource
-from examples.helpers import D_TYPE, print_time_info, load_default_topology, FLAGS
+from alphai_crocubot_oracle.helpers import printtime, execute_and_get_duration
+
+from examples.helpers import D_TYPE, load_default_topology, FLAGS
+from examples.benchmark.helpers import print_time_info, print_accuracy, _calculate_accuracy
 
 
 def run_timed_benchmark_time_series(series_name, flags, do_training=True):
@@ -20,57 +22,76 @@ def run_timed_benchmark_time_series(series_name, flags, do_training=True):
     topology = load_default_topology(series_name)
 
     #  First need to establish bin edges using full training set
-    template_sample_size = np.minimum(flags.n_training_samples_benchmark, 10000)
+    n_train_samples = np.minimum(flags.n_training_samples_benchmark, 10000)
 
-    batch_options = BatchOptions(batch_size=template_sample_size,
-                                 batch_number=0,
-                                 train=do_training,
-                                 dtype=D_TYPE)
+    bin_distribution = _create_bin_distribution(series_name, n_train_samples, topology)
+    batch_size = FLAGS.batch_size
 
-    data_source = data_source_generator.make_data_source(series_name)
+    @printtime(message="Training {} with do_train: {}".format(series_name, int(do_training)))
+    def _do_training():
+        execution_time = datetime.datetime.now()
+        if do_training:
 
-    _, labels = train_with_datasource.get_batch_from_generator(batch_options, data_source)
+            data_provider = TrainDataProviderForDataSource(
+                series_name,
+                D_TYPE,
+                n_train_samples,
+                batch_size,
+                True,
+                bin_distribution.bin_edges
+            )
+            save_path = io.build_check_point_filename(series_name, topology)
+            tensorflow_path = TensorflowPath(save_path, FLAGS.model_save_path)
+            tensorboard_options = TensorboardOptions(FLAGS.tensorboard_log_path,
+                                                     FLAGS.learning_rate,
+                                                     batch_size,
+                                                     execution_time
+                                                     )
+            crocubot_train.train(topology,
+                                 data_provider,
+                                 tensorflow_path,
+                                 tensorboard_options
+                                 )
+        else:
+            tf.reset_default_graph()
+            model = CrocuBotModel(topology)
+            model.build_layers_variables()
 
-    bin_dist = BinDistribution(labels, topology.n_classification_bins)
+    train_time, _ = execute_and_get_duration(_do_training)
 
-    start_time = timer()
-
-    execution_time = datetime.datetime.now()
-
-    if do_training:
-        train_with_datasource.train_with_datasource(topology, series_name, execution_time, bin_edges=bin_dist.bin_edges)
-    else:
-        tf.reset_default_graph()
-        model = CrocuBotModel(topology)
-        model.build_layers_variables()
-
-    mid_time = timer()
-    train_time = mid_time - start_time
     print("Training complete.")
 
-    evaluate_network(topology, series_name, bin_dist)
-    eval_time = timer() - mid_time
+    eval_time, _ = execute_and_get_duration(evaluate_network, topology, series_name, batch_size, bin_distribution)
 
     print('Metrics:')
     print_time_info(train_time, eval_time)
 
 
-data_source_generator = DataSourceGenerator()
+def _create_bin_distribution(series_name, n_training_samples, topology):
+    data_provider = TrainDataProviderForDataSource(series_name, D_TYPE, n_training_samples, n_training_samples, True)
+    train_data = data_provider.get_batch(0)
+
+    return BinDistribution(train_data.labels, topology.n_classification_bins)
 
 
-def evaluate_network(topology, series_name, bin_dist):  # bin_dist not used in MNIST case
-    data_provider = TrainDataProviderForDataSource(series_name, D_TYPE, FLAGS.batch_size * 2, False)
+@printtime(message="Evaluation of Stocastic Serie")
+def evaluate_network(topology, series_name, batch_size, bin_dist):
 
-    test_features, test_labels = data_provider.get_batch(1, FLAGS.batch_size)
+    n_training_samples = batch_size * 2
+    data_provider = TrainDataProviderForDataSource(series_name, D_TYPE, n_training_samples, batch_size, False)
 
+    test_features, test_labels = data_provider.get_batch(1)
     save_file = io.build_check_point_filename(series_name, topology)
 
-    binned_outputs = eval.eval_neural_net(test_features, topology, save_file)
-    n_samples = binned_outputs.shape[1]
+    binned_outputs = crocubot_eval.eval_neural_net(test_features, topology, save_file)
 
-    model_metrics = Metrics()
-
-    estimated_means, estimated_covariance = eval.forecast_means_and_variance(binned_outputs, bin_dist)
+    estimated_means, estimated_covariance = crocubot_eval.forecast_means_and_variance(binned_outputs, bin_dist)
     test_labels = np.squeeze(test_labels)
 
-    model_metrics.evaluate_sample_performance(data_provider.data_source, test_labels, estimated_means, estimated_covariance)
+    model_metrics = Metrics()
+    model_metrics.evaluate_sample_performance(
+        data_provider.data_source,
+        test_labels,
+        estimated_means,
+        estimated_covariance
+    )
