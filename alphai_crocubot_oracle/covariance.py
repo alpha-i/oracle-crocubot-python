9aea5be18faa1d362ead43e2313aed3ede978083
Fergus Simpson <fergus.simpson@alpha-i.co>
2017-11-08 14:35:22 +0000

    Added protection to covariance estimator against unusually large datapoints

--- alphai_crocubot_oracle/covariance.py
+++ alphai_crocubot_oracle/covariance.py
@@ -1,15 +1,18 @@
+from copy import deepcopy
 import numpy as np
-from alphai_covariance.dynamic_cov import estimate_cov
+import logging
 
+from alphai_covariance.dynamic_cov import estimate_cov
 from alphai_crocubot_oracle.data.cleaning import sample_minutes_after_market_open_data_frame
 
 DEFAULT_N_ESTIMATES = 100
 DEFAULT_SPLIT_STEPS = 1
 DEFAULT_NUM_REALISATIONS_MULTIPLICATION_FACTOR = 3
+MAX_LOG_RETURN_AMPLITUDE = 1
 
 
-def estimate_covariance(data, ndays, minutes_after_open, estimation_method,
-                        exchange_calendar, forecast_interval_in_days, target_symbols=None):
+def estimate_covariance(data, n_days, minutes_after_open, estimation_method,
+                                  exchange_calendar, forecast_interval_in_days, target_symbols=None):
     """
     :param data: OHLCV data
     :param ndays: number of historical days expected for the covariance estimate
@@ -27,14 +30,32 @@ def estimate_covariance(data, ndays, minutes_after_open, estimation_method,
     if target_symbols is not None:
         data = data[target_symbols]
 
-    nd = data.shape[1]
-    sampling_days = np.maximum(ndays, nd * DEFAULT_NUM_REALISATIONS_MULTIPLICATION_FACTOR)
+    n_dimensions = data.shape[1]
+    sampling_days = np.maximum(n_days, n_dimensions * DEFAULT_NUM_REALISATIONS_MULTIPLICATION_FACTOR)
     data_points = data.values[-sampling_days:, :]
+    data_points = clip_data(data_points)
 
     covariance_matrix, _ = estimate_cov(data_points, method=estimation_method, is_dynamic=False)
 
     # Rescale amplitude for longer time horizons
     return covariance_matrix * forecast_interval_in_days
+
+
+def clip_data(x_data):
+    """ Reduce outliers to some maximum amplitude. """
+
+    flat_x = deepcopy(x_data).flatten()
+    max_data = np.max(np.abs(flat_x))
+
+    if max_data > MAX_LOG_RETURN_AMPLITUDE:
+        x_data = np.clip(x_data, a_min=-MAX_LOG_RETURN_AMPLITUDE, a_max=MAX_LOG_RETURN_AMPLITUDE)
+
+        n_clipped_elements = np.sum(MAX_LOG_RETURN_AMPLITUDE < np.abs(flat_x))
+        n_elements = len(flat_x)
+        logging.warning("Large variance detected: clip values exceeding {}".format(MAX_LOG_RETURN_AMPLITUDE))
+        logging.info("{} of {} datapoints were clipped.".format(n_clipped_elements, n_elements))
+
+    return x_data
 
 
 def returns_minutes_after_market_open_data_frame(data_frame, market_calendar, minutes_after_market_open):
