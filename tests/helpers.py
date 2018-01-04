84ffd6a2cafc8035e0045010422fd7c93f845170
Daniele Murroni <daniele.murroni@gmail.com>
2018-01-04 14:00:50 +0000

    Dev/alcova target (#100)
    * Crocubot now returns prediction timestamp and it is able to (#98)
    
    * Crocubot now returns prediction timestamp and it is able to
    perform the eod prediction according to the configuration parameter
    
    `predict_the_market_close`
    
    Development branches for feature-generation and delphi have been merged
    into develop since.
    
    * Encapsulating initialization lines
    Building Universe provider exchange using the feature transformation `exchange` parameter

--- tests/helpers.py
+++ tests/helpers.py
@@ -81,7 +81,7 @@ def default_oracle_config():
                     'local': False
                 },
             ],
-            'exchange_name': 'NYSE',
+            'exchange': 'NYSE',
             'features_ndays': 10,
             'features_resample_minutes': 15,
             'fill_limit': 5,
