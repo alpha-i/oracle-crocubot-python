fix error intrudced during rebase on covariance calculation

alphai_crocubot_oracle/oracle.py
@@ -315,7 +315,7 @@ class CrocubotOracle(AbstractOracle):
             logging.info('Samples from historical covariance: {}'.format(np.diag(covariance_matrix)[0:5]))
             logging.warning('Invoking temporary covariance hack')
             cov_diag = np.diag(covariance_matrix) + 1e-4
-            covariance = np.diag(cov_diag)
+            covariance_matrix = np.diag(cov_diag)
         else:
             covariance_matrix = forecast_covariance
             logging.info("Samples from forecast_covariance: {}".format(np.diag(covariance_matrix)[0:5]))