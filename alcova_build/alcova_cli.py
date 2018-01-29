ef14c4c908de811109f4bb42023f94282fcf2732
Daniele Murroni <daniele.murroni@gmail.com>
2018-01-29 16:13:26 +0100

    Encapsulate initialization logic
    Now the logic to initialize variables to be used in the CLI
    is wrapped inside a class called initializer. This will make
    easier import multiple compiled alcova_init modules to run
    an averager.

--- alcova_build/alcova_cli.py
+++ alcova_build/alcova_cli.py
@@ -1,4 +1,11 @@
-from alcova_init import controller
+import os
+from alcova_init import Initializer
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == '__main__':
-    controller.run()
+    initializer = Initializer(BASE_DIR)
+    with open(os.path.join(BASE_DIR, 'alcova.yml')) as alcova_config_file:
+        initializer.initialize(alcova_config_file)
+        initializer.controller.run()
--- alcova_build/alcova_init.tpl.py
+++ alcova_build/alcova_init.tpl.py
@@ -16,65 +16,77 @@ from alphai_crocubot_oracle.oracle import CrocubotOracle
 
 LOG_FORMAT = '%(asctime)s - %(levelname)s [%(name)s:%(module)s:%(lineno)d]: %(message)s'
 DATE_FORMAT = '%Y-%m-%d'
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
 
 logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
 
-with open(os.path.join(BASE_DIR, 'alcova.yml')) as alcova_config_file:
-    alcova_config = yaml.load(alcova_config_file)
-
 EXCHANGE_NAME = "JPX"
-simulation_start = pytz.utc.localize(datetime.strptime(alcova_config['simulation_start'], DATE_FORMAT))
-simulation_end = pytz.utc.localize(datetime.strptime(alcova_config['simulation_end'], DATE_FORMAT))
+
 run_name = 'alcova'
 
-RUNTIME_DIR_PATH = os.path.join(BASE_DIR, 'runtime')
-RESULT_DIRECTORY = os.path.join(BASE_DIR, 'result')
+class Initializer:
+    def __init__(self, base_dir):
 
-datasource_configuration = {
-    'feature_mapping': {
-        'close': alcova_config['data_files']['close'],
-        'volume': alcova_config['data_files']['volume']
-    },
-    'adjustments_file': alcova_config['data_files']['adjustments']
-}
+        self.base_dir = base_dir
+        self.controller_config = None
+
+        self.alcova_config_file_path = None
+        self.datasource = None
+        self.alcova_config = None
+
+        self.oracle = None
+        self.oracle_performance = None
+
+        self.scheduler = None
+        self.controller = None
+
+        RUNTIME_DIR_PATH = os.path.join(self.base_dir, 'runtime')
+
+        self.oracle_config = %%ORACLE_CONFIG%%
+
+        self.scheduling = %%SCHEDULE_CONFIG%%
+
+    def initialize(self, alcova_config):
+
+        self.alcova_config = yaml.load(alcova_config)
+
+        datasource_configuration = {
+            'feature_mapping': {
+                'close': self.alcova_config['data_files']['close'],
+                'volume': self.alcova_config['data_files']['volume']
+            },
+            'adjustments_file': self.alcova_config['data_files']['adjustments']
+        }
+
+        logging.info("Loading datasource ...")
+        self.datasource = AlcovaDataSource(datasource_configuration)
+        logging.info("Datasource Loaded!")
+
+        self.oracle = CrocubotOracle(OracleConfiguration({"scheduling": self.scheduling,
+                                                          "oracle": self.oracle_config}))
+
+        simulation_start = pytz.utc.localize(datetime.strptime(self.alcova_config['simulation_start'], DATE_FORMAT))
+        simulation_end = pytz.utc.localize(datetime.strptime(self.alcova_config['simulation_end'], DATE_FORMAT))
+        self.scheduler = Scheduler(simulation_start,
+                                   simulation_end,
+                                   EXCHANGE_NAME,
+                                   self.oracle.prediction_frequency,
+                                   self.oracle.training_frequency,
+                                   self.oracle.prediction_horizon)
+
+        RESULT_DIRECTORY = os.path.join(self.base_dir, 'result')
+        self.oracle_performance = OraclePerformance(os.path.join(RESULT_DIRECTORY), run_name)
+
+        self.controller_config = {
+            'start_date': self.alcova_config['simulation_start'],
+            'end_date': self.alcova_config['simulation_end']
+        }
+
+        self.controller = Controller(
+            configuration=ControllerConfiguration(self.controller_config),
+            oracle=self.oracle,
+            scheduler=self.scheduler,
+            datasource=self.datasource,
+            performance=self.oracle_performance
+        )
 
 
-###########################################
-#         RUNTIME CONFIGURATIONS          #
-###########################################
-
-controller_config = {
-    'start_date': alcova_config['simulation_start'],
-    'end_date': alcova_config['simulation_end']
-}
-
-scheduling = %%SCHEDULE_CONFIG%%
-
-###########################################
-#      ALPHA-I ORACLE CONFIGURATION       #
-###########################################
-
-N_ASSETS = 400
-
-oracle_config = %%ORACLE_CONFIG%%
-
-logging.info("Loading datasource ...")
-datasource = AlcovaDataSource(datasource_configuration)
-logging.info("Datasource Loaded!")
-
-oracle = CrocubotOracle(OracleConfiguration({"scheduling": scheduling, "oracle": oracle_config}))
-
-scheduler = Scheduler(simulation_start, simulation_end, EXCHANGE_NAME,
-                      oracle.prediction_frequency, oracle.training_frequency, oracle.prediction_horizon)
-
-oracle_performance = OraclePerformance(os.path.join(RESULT_DIRECTORY), run_name)
-
-controller = Controller(
-    configuration=ControllerConfiguration(controller_config),
-    oracle=oracle,
-    scheduler=scheduler,
-    datasource=datasource,
-    performance=oracle_performance
-)
